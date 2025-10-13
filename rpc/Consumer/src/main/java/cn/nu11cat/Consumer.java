a02ed2982aac460fb3f59e5f519956f0dd39b847
Nu11Cat <2111867383@qq.com>
2025-10-13 16:49:53 +0800

    feat:动态代理优化消费者服务调用

--- rpc/Consumer/src/main/java/cn/nu11cat/Consumer.java
+++ rpc/Consumer/src/main/java/cn/nu11cat/Consumer.java
@@ -2,17 +2,12 @@ package cn.nu11cat;
 
 import cn.nu11cat.common.Invocation;
 import cn.nu11cat.protocol.HttpClient;
+import cn.nu11cat.proxy.ProxyFactory;
 
 public class Consumer {
     public static void main(String[] args) {
-//        HelloService helloService = ?;
-//        String result = helloService.sayHello("world");
-//        System.out.println(result);
-
-        Invocation invocation = new Invocation(HelloService.class.getName(), "sayHello", new Class[]{String.class}, new Object[]{"nu11cat"});
-
-        HttpClient httpClient = new HttpClient();
-        String result = httpClient.send("localhost", 8080, invocation);
+        HelloService helloService = ProxyFactory.getProxy(HelloService.class);
+        String result = helloService.sayHello("nu11cat2");
         System.out.println(result);
 
     }
---
+++ rpc/nu11rpc/src/main/java/cn/nu11cat/proxy/ProxyFactory.java
@@ -0,0 +1,28 @@
+package cn.nu11cat.proxy;
+
+import cn.nu11cat.common.Invocation;
+import cn.nu11cat.protocol.HttpClient;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyFactory {
+
+    public static <T> T getProxy(Class interfaceClass) {
+
+        Object proxyInstance = Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[]{interfaceClass}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Invocation invocation = new Invocation(interfaceClass.getName(), method.getName(), method.getParameterTypes(), args);
+
+                HttpClient httpClient = new HttpClient();
+                String result = httpClient.send("localhost", 8080, invocation);
+
+                return result;
+            }
+        });
+
+        return (T) proxyInstance;
+    }
+}
